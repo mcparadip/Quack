1f75f2bb2aed1a554071272f2684d4b67df59cc4
Oliver Ni <oliver.ni@gmail.com>
2017-05-09 21:29:45 -0700

    Fixed quilt micromatch

--- micromatch.js
+++ micromatch.js
@@ -1,13 +1,24 @@
 chrome.storage.sync.get("set", function(items) {
   if (items.set != undefined && items.set != "") {
-    var cardids = document.querySelectorAll(".text")
+    try {
+      document.querySelectorAll(".UIButton")[1].click()
+    } catch(err) {}
+
+    var cardids = Array.from(document.querySelectorAll(".text"))
+
+    var cards = {}
+
     for (var i=0; i<cardids.length; i++) {
       cards[cardids[i].innerText] = cardids[i]
     }
-    document.querySelectorAll(".UIButton")[1].click()
+
     while (cardids.length > 0) {
       cardids[0].click()
-      var target = cards[itemes.set[cardids[0].innerText]]
+      console.log(items.set)
+      console.log(cardids[0].innerText)
+      console.log(items.set[cardids[0].innerText])
+      console.log(cards)
+      var target = cards[items.set[cardids[0].innerText]]
       target.click()
       cardids.splice(0, 1)
       var index = cardids.indexOf(target)
--- set.js
+++ set.js
@@ -1,10 +1,14 @@
 var termids = document.querySelectorAll(".TermText")
+
+console.log(termids.length)
 var terms = {}
 
-for (var i=0; i<termids.length/2; i++) {
+for (var i=0; i<termids.length; i+=2) {
     terms[termids[i].innerText] = termids[i+1].innerText
     terms[termids[i+1].innerText] = termids[i].innerText
 }
+
+console.log(Object.keys(terms).length)
 
 chrome.storage.sync.set({"set": terms}, function() {
     alert("This set has been loaded! You are now ready to play.")
